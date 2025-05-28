f6d153426bbac28e2d36037fc4b3786e6b14dba9
Arthur Monteiro <monteiroamelo@gmail.com>
2025-05-28 10:04:52 -0300

    feat: lints from sonar source

--- sonar_scraper/bin/sonar_scraper.dart
+++ sonar_scraper/bin/sonar_scraper.dart
@@ -3,10 +3,13 @@ import 'package:sonar_scraper/sonar_scraper.dart' as sonar_scraper;
 void main() async {
   final links = await sonar_scraper.getRulesLinks();
   final rules = <String>[];
-  for (final link in links.sublist(0, 10)) {
+  for (final link in links) {
     final rule = await sonar_scraper.getRuleByUrl(link);
     if (rule != null) {
       rules.add(rule);
     }
   }
+
+  await sonar_scraper.updateLinterRules(rules);
+  print('Linter rules updated successfully!');
 }
